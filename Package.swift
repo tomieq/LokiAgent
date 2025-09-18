a4ad6ee7ba6d99a2faa50d355c67c7f4bc4f901b
tomieq <tomieq@wp.pl>
2025-09-18 12:16:34 +0200

    Added support for iOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "LokiAgent",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v14),
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
